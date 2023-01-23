3e3971d58e313a3f6e49adfb612cfa33f5d1c896
Brendan Fuller <git@brendanfuller.com>
2023-01-23 12:54:12 -0500

    🐛 Swap Argon2 to brcrypt for password hashing

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -1,18 +1,18 @@
 import { signUpSchema } from "src/validation/auth";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
-import { hash } from "argon2";
+import { hash } from "bcrypt";
 
 export const routerAuth = createTRPCRouter({
   signUp: publicProcedure
     .input(signUpSchema)
     .mutation(async ({ input, ctx }) => {
       const { username, email, password } = input;
-
+      console.log("Find the user?");
       const exists = await ctx.prisma.user.findFirst({
         where: { email },
       });
-
+      console.log("Did we get here?");
       if (exists) {
         return {
           status: 409,
@@ -21,12 +21,13 @@ export const routerAuth = createTRPCRouter({
         };
       }
 
-      const hashedPassword = await hash(password);
-
+      const hashedPassword = await hash(password, 10);
+      console.log("Did we get to make the user?");
       const result = await ctx.prisma.user.create({
         data: { username, email, password: hashedPassword },
       });
 
+      console.log("Did we return the info");
       return {
         status: 201,
         message: "Account created successfully",

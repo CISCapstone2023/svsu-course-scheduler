e849b5acf2ac7c649d2d3c03147d0ee740f3af28
Hawkins Amos <70655939+SirAwesome-5@users.noreply.github.com>
2023-01-31 13:28:32 -0500

    Added add, search, delete for buildings

--- src/server/api/routers/buildings.ts
+++ src/server/api/routers/buildings.ts
@@ -5,8 +5,15 @@ import {
   publicProcedure,
   protectedProcedure,
 } from "src/server/api/trpc";
-import { GuidelineCampus, GuidelinesCourses } from "@prisma/client";
-import { createCampusSchema } from "src/validation/buildings";
+import {
+  GuidelineBuilding,
+  GuidelineCampus,
+  GuidelinesCourses,
+} from "@prisma/client";
+import {
+  createCampusSchema,
+  createBuildingSchema,
+} from "src/validation/buildings";
 
 export const buildingsRouter = createTRPCRouter({
   //Get all campus and return the list of campus
@@ -80,6 +87,95 @@ export const buildingsRouter = createTRPCRouter({
 
       return {
         campus,
+      };
+    }),
+
+  // Buildings
+
+  getAllBuildings: protectedProcedure
+    .input(
+      z.object({
+        search: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      let buildingResult: GuidelineBuilding[] = [];
+
+      //Do we have a search query
+      if (input.search != "") {
+        buildingResult = await ctx.prisma.guidelineBuilding.findMany({
+          take: 10,
+          skip: 0,
+          where: {
+            name: {
+              contains: input.search,
+            },
+          },
+        });
+      } else {
+        //If we don't have a search query don't worry about the filter
+        buildingResult = await ctx.prisma.guidelineBuilding.findMany({
+          take: 10,
+          skip: 0,
+        });
+      }
+
+      //Return the data
+      return buildingResult;
+    }),
+
+  // UNIFINISHED ---------------------
+  addBuilding: protectedProcedure
+    .input(createBuildingSchema)
+    .mutation(async ({ ctx, input }) => {
+      //Add a new building
+      const building = await ctx.prisma.guidelineBuilding.create({
+        data: {
+          name: input.name,
+          prefix: input.prefix.toUpperCase(),
+        },
+      });
+
+      console.log(building);
+
+      return {
+        result: building,
+      };
+    }),
+
+  deleteBuilding: protectedProcedure
+    .input(
+      z.object({
+        tuid: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const building = await ctx.prisma.guidelineBuilding.delete({
+        where: {
+          tuid: input.tuid,
+        },
+      });
+
+      return {
+        result: building,
+      };
+    }),
+
+  getBuilding: protectedProcedure
+    .input(
+      z.object({
+        tuid: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const building = await ctx.prisma.guidelineCampus.findUnique({
+        where: {
+          tuid: input.tuid,
+        },
+      });
+
+      return {
+        result: building,
       };
     }),
 });

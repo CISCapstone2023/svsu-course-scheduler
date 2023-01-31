e0ef0c5db5351e7630629940803305dddfab7134
Hawkins Amos <70655939+SirAwesome-5@users.noreply.github.com>
2023-01-31 13:27:28 -0500

    Added zod object for creating buildings

--- src/validation/buildings.ts
+++ src/validation/buildings.ts
@@ -4,4 +4,10 @@ export const createCampusSchema = z.object({
   name: z.string().min(4).max(30),
 });
 
+export const createBuildingSchema = z.object({
+  name: z.string().min(4).max(75),
+  prefix: z.string().min(1).max(4),
+});
+
 export type ICreateCampus = z.infer<typeof createCampusSchema>;
+export type ICreateBuilding = z.infer<typeof createBuildingSchema>;

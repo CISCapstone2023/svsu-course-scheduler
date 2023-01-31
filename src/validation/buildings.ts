f66a97b3df17a2698ed4bec879be3c8ceab1fa0b
Brendan Fuller <git@brendanfuller.com>
2023-01-31 12:43:47 -0500

    Added deleting and updating fields

--- src/validation/buildings.ts
+++ src/validation/buildings.ts
@@ -1,7 +1,15 @@
 import { z } from "zod";
 
 export const createCampusSchema = z.object({
-  name: z.string().min(4).max(30),
+  name: z
+    .string()
+    .min(3, { message: "Campus name must be at least 3 characters" })
+    .max(30),
+});
+
+export const updateCampusSchema = createCampusSchema.extend({
+  tuid: z.string(),
 });
 
 export type ICreateCampus = z.infer<typeof createCampusSchema>;
+export type IUpdateCampus = z.infer<typeof updateCampusSchema>;

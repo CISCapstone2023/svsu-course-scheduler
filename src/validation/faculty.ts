379fc0c97c98656c42ffb195e2bec3a72a255626
Chris Bellefeuille <87543531+Chris-Bellefeuille-SVSU@users.noreply.github.com>
2023-01-31 12:02:10 -0500

    📝 Added comments for the schemas

--- src/validation/faculty.ts
+++ src/validation/faculty.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 
+//zod faculty schema without tuid for reusability in api's
 export const createFacultySchemaNoTUID = z.object({
+  //use all of GuidlelinesFactuly's fields except tuid
   suffix: z.string(),
   last_name: z.string(),
   first_name: z.string(),
@@ -8,6 +10,8 @@ export const createFacultySchemaNoTUID = z.object({
   is_adjunct: z.boolean(),
 });
 
+//zod faculty schema with tuid added for reusability in api's
 export const createFacultySchemaWithTUID = createFacultySchemaNoTUID.extend({
+  //after extending from the above schema, add tuid to this one
   tuid: z.string(),
 });

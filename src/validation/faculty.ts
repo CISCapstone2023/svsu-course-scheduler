2a1cb28b8c45a65d384be4633101be1fca94512d
Chris Bellefeuille <87543531+Chris-Bellefeuille-SVSU@users.noreply.github.com>
2023-02-02 11:40:12 -0500

    ✨ Added a zod schema to get only the faculty tuid

--- src/validation/faculty.ts
+++ src/validation/faculty.ts
@@ -1,5 +1,10 @@
 import { z } from "zod";
 
+//zid faculty schema that just grabs the TUID of faculty
+export const createFacultySchemaTUID = z.object({
+  tuid: z.string(),
+});
+
 //zod faculty schema without tuid for reusability in api's
 export const createFacultySchemaNoTUID = z.object({
   //use all of GuidlelinesFactuly's fields except tuid

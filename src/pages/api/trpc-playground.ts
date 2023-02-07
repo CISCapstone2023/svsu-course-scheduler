608e77240547dec8f00c080a113d7386108cce8a
Samuel Johnson <sgjohns1@svsu.edu>
2023-02-07 13:18:43 -0500

    Add trpc playground.

---
+++ src/pages/api/trpc-playground.ts
@@ -0,0 +1,21 @@
+import { NextApiHandler } from "next";
+import { appRouter } from "src/server/api/root";
+import { nextHandler } from "trpc-playground/handlers/next";
+
+const setupHandler = nextHandler({
+  router: appRouter,
+  // tRPC api path, pages/api/trpc/[trpc].ts in this case
+  trpcApiEndpoint: "/api/trpc",
+  playgroundEndpoint: "/api/trpc-playground",
+  //uncomment this if you're using superjson
+  request: {
+    superjson: true,
+  },
+});
+
+const handler: NextApiHandler = async (req, res) => {
+  const playgroundHandler = await setupHandler;
+  await playgroundHandler(req, res);
+};
+
+export default handler;

ce1465c061d58fa8e3daa2617eea3867d5841d0b
cooperL0 <cooperlenzi0@gmail.com>
2023-02-14 11:24:04 -0500

    ✨ Starting validation for parsing back to excel

--- src/validation/projects.ts
+++ src/validation/projects.ts
@@ -2,27 +2,54 @@ import { z } from "zod";
 
 //export const originalStateSchema = z.object({});
 
-export const courseDatabaseSchema = z.object({
-  type: z.string().optional(),
-  section_id: z.string().optional(),
-  term: z.string().optional(),
-  div: z.string().optional(),
-  department: z.string().optional(),
-  subject: z.string().optional(),
-  course_number: z.string().optional(),
-  section: z.string().optional(),
-  start_date: z.string().optional(),
-  end_date: z.string().optional(),
-  start_time: z.string().optional(),
-  end_time: z.string().optional(),
-  capacity: z.string().optional(),
-  credits: z.string().optional(),
-  title: z.string().optional(),
-  building: z.string().optional(),
-  faculty: z.string().optional(),
-  instruction_method: z.string().optional(),
-  campus: z.string().optional(),
-  days: z.string().optional(),
+// export const courseDatabaseSchema = z.object({
+//   type: z.string().optional(),
+//   section_id: z.string().optional(),
+//   term: z.string().optional(),
+//   div: z.string().optional(),
+//   department: z.string().optional(),
+//   subject: z.string().optional(),
+//   course_number: z.string().optional(),
+//   section: z.string().optional(),
+//   start_date: z.string().optional(),
+//   end_date: z.string().optional(),
+//   start_time: z.string().optional(),
+//   end_time: z.string().optional(),
+//   capacity: z.string().optional(),
+//   credits: z.string().optional(),
+//   title: z.string().optional(),
+//   building: z.string().optional(),
+//   faculty: z.string().optional(),
+//   instruction_method: z.string().optional(),
+//   campus: z.string().optional(),
+//   days: z.string().optional(),
+// });
+
+export const courseDatabaseSchemaV2 = z.object({
+  type: z.string(),
+  section_id: z.preprocess((x) => parseInt(String(x)), z.number()),
+  term: z.preprocess((x) => parseInt(String(x.substring(0, 2))), z.number()),
+  //check format?
+  div: z.string(),
+  department: z.string(),
+  subject: z.string(),
+  //
+  course_number: z.string(),
+  section: z.string(),
+  // parse string to date
+  start_date: z.string(),
+  end_date: z.string(),
+  //make sure
+  start_time: z.number(),
+  end_time: z.number(),
+  capacity: z.number(),
+  credits: z.number(),
+  title: z.string(),
+  building: z.string(),
+  faculty: z.string(),
+  instruction_method: z.string(),
+  campus: z.string(),
+  days: z.string(),
 });
 
 export const organizeColumns = z.object({

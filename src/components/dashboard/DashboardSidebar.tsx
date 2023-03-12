473110292d8baf7e2f940676aca794189f1d98eb
Saturn <25212998+Shadow12587@users.noreply.github.com>
2023-03-12 18:49:28 -0400

    ✅ Added clear and concise comments to sidebar code

--- src/components/dashboard/DashboardSidebar.tsx
+++ src/components/dashboard/DashboardSidebar.tsx
@@ -18,6 +18,7 @@ interface DashboardSidebarProps {
   children?: React.ReactNode;
 }
 
+// Dashboard prop from DaisyUI
 const DashboardSidebar = ({ children }: DashboardSidebarProps) => {
   const router = useRouter();
   const { scheduleId } = router.query;
@@ -28,6 +29,7 @@ const DashboardSidebar = ({ children }: DashboardSidebarProps) => {
 
   return (
     <div className="flex h-full w-[220px] flex-col bg-base-200 pt-4">
+      {/* Displays name of project youre on */}
       <Menu>
         <Link href={`/dashboard/${scheduleId}/home`}>
           <DashboardSidebarItem
@@ -40,21 +42,26 @@ const DashboardSidebar = ({ children }: DashboardSidebarProps) => {
 
         <Divider />
 
+        {/* Header for general section */}
         <div className="flex w-full pl-4">
           <p className="font-bold">General</p>
         </div>
+
+        {/* Redirects to home page when clicked */}
         <Link href={`/dashboard/${scheduleId}/home`}>
           <DashboardSidebarItem title="Home">
             <Home width={40} height={40} />
           </DashboardSidebarItem>
         </Link>
 
+        {/* Redirects to schedule page when clicked */}
         <Link href={`/dashboard/${scheduleId}/schedule`}>
           <DashboardSidebarItem title="Schedule">
             <Calendar width={40} height={40} />
           </DashboardSidebarItem>
         </Link>
 
+        {/* Redirects to report page when clicked */}
         <Link href={`/dashboard/${scheduleId}/report`}>
           <DashboardSidebarItem title="Report">
             <ChartBar width={40} height={40} />
@@ -63,20 +70,26 @@ const DashboardSidebar = ({ children }: DashboardSidebarProps) => {
 
         <Divider />
 
+        {/* Header for admin section */}
         <div className="flex w-full pl-4">
           <p className="font-bold">Admin</p>
         </div>
+
+        {/* Redirects to courses page when clicked */}
         <Link href={`/dashboard/${scheduleId}/courses`}>
           <DashboardSidebarItem title="Courses">
             <Book width={40} height={40} />
           </DashboardSidebarItem>
         </Link>
 
+        {/* Redirects to faculty page when clicked */}
         <Link href={`/dashboard/${scheduleId}/faculty`}>
           <DashboardSidebarItem title="Faculty">
             <User width={40} height={40} />
           </DashboardSidebarItem>
         </Link>
+
+        {/* Redirects to buildings page when clicked */}
         <Link href={`/dashboard/${scheduleId}/buildings`}>
           <DashboardSidebarItem title="Buildings">
             <Building width={40} height={40} />
@@ -87,4 +100,5 @@ const DashboardSidebar = ({ children }: DashboardSidebarProps) => {
   );
 };
 
+//Exports component with memorization
 export default React.memo(DashboardSidebar);

e2bcdd584c8b213a9535ca2b63be6f5f8b275331
Saturn <25212998+Shadow12587@users.noreply.github.com>
2023-03-12 19:53:20 -0400

    ✅ Added comment

--- src/components/dashboard/DashboardLayout.tsx
+++ src/components/dashboard/DashboardLayout.tsx
@@ -4,6 +4,7 @@ interface DashboardProps {
   children?: React.ReactNode;
 }
 
+// Ensures all children have flex
 const DashboardLayout = ({ children }: DashboardProps) => {
   return (
     <div className="flex h-full w-full bg-primary-content">{children}</div>

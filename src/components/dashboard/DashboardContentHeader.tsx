255bab94e4b7c84d95ef26f92ea250a85f9ef212
Brendan Fuller <git@brendanfuller.com>
2023-03-29 13:02:30 -0400

    ✅ Added menu item for toggle

--- src/components/dashboard/DashboardContentHeader.tsx
+++ src/components/dashboard/DashboardContentHeader.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Breadcrumbs } from "react-daisyui";
+import { Menu, Menu2 } from "tabler-icons-react";
 
 interface DashboardContentHeader {
   children?: React.ReactNode;
   title: string;
+  onMenuClick?: () => void;
 }
 
 /**
@@ -15,10 +17,19 @@ interface DashboardContentHeader {
 const DashboardContentHeader = ({
   children,
   title,
+  onMenuClick,
 }: DashboardContentHeader) => {
   return (
-    <div className="border-gray align-center flex h-[50px] w-full justify-center justify-between border-b-[1px] p-2 ">
+    <div className="border-gray align-center flex min-h-[50px] w-full justify-center justify-between border-b-[1px] p-2 ">
       <div className="my-auto flex">
+        <Menu2
+          className="mr-5 w-[25px] rounded-md p-1 hover:cursor-pointer hover:bg-base-200"
+          onClick={() => {
+            if (onMenuClick) {
+              onMenuClick();
+            }
+          }}
+        />
         <h3 className="font-bold">{title}</h3>
       </div>
       <div className="">{children}</div>
